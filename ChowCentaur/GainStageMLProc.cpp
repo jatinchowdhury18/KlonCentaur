7dbc1ea32a5fb0f409f752d4f1c3d452d5513520
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2020-08-15 15:39:39 -0700

    Add both models to plugin

--- ChowCentaur/GainStageMLProc.cpp
+++ ChowCentaur/GainStageMLProc.cpp
@@ -2,17 +2,24 @@
 
 GainStageMLProc::GainStageMLProc (AudioProcessorValueTreeState& vts)
 {
-    MemoryInputStream jsonInput0 (BinaryData::centaur_json, BinaryData::centaur_jsonSize, false);
-    MemoryInputStream jsonInput1 (BinaryData::centaur_json, BinaryData::centaur_jsonSize, false);
-
-    Json2RnnParser parser;
-    gainStageML[0] = parser.parseJson (jsonInput0);
-    gainStageML[1] = parser.parseJson (jsonInput1);
-
-    jassert (gainStageML[0].get() != nullptr);
-    jassert (gainStageML[1].get() != nullptr);
+    loadModel (gainStageML, BinaryData::centaur_json, BinaryData::centaur_jsonSize);
+    loadModel (gainStageMLLarge, BinaryData::centaur_large_json, BinaryData::centaur_large_jsonSize);
 
     gainParam = vts.getRawParameterValue ("gain");
+    mlParam     = vts.getRawParameterValue ("neural");
+}
+
+void GainStageMLProc::loadModel (ModelPtr model[2], const char* data, int size)
+{
+    MemoryInputStream jsonInput0 (data, size, false);
+    MemoryInputStream jsonInput1 (data, size, false);
+
+    Json2RnnParser parser;
+    model[0] = parser.parseJson (jsonInput0);
+    model[1] = parser.parseJson (jsonInput1);
+
+    jassert (model[0].get() != nullptr);
+    jassert (model[1].get() != nullptr);
 }
 
 void GainStageMLProc::reset (double sampleRate, int samplesPerBlock)
@@ -20,13 +27,20 @@ void GainStageMLProc::reset (double sampleRate, int samplesPerBlock)
     ignoreUnused (samplesPerBlock);
 
     for (int ch = 0; ch < 2; ++ch)
+    {
         gainStageML[ch]->reset();
+        gainStageMLLarge[ch]->reset();
+    }
 
     T = 1.0f / (float) sampleRate;
 }
 
 void GainStageMLProc::processBlock (AudioBuffer<float>& buffer)
 {
+    auto model = gainStageML;
+    if (*mlParam == 2)
+        model = gainStageMLLarge;
+
     for (int ch = 0; ch < buffer.getNumChannels(); ++ch)
     {
         auto* x = buffer.getWritePointer (ch);
@@ -34,7 +48,7 @@ void GainStageMLProc::processBlock (AudioBuffer<float>& buffer)
         for (int n = 0; n < buffer.getNumSamples(); ++n)
         {
             float input[] = { x[n], *gainParam, T };
-            x[n] = gainStageML[ch]->forward (input);
+            x[n] = model[ch]->forward (input);
         }
     }
 }
