a9415224002c39ddc24972785e1c3bdfe9a39825
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2020-08-16 09:55:26 -0700

    DRY Bilinear transform code

--- SharedCode/OutputStageProcessor.cpp
+++ SharedCode/OutputStageProcessor.cpp
@@ -1,4 +1,5 @@
 #include "OutputStageProcessor.h"
+#include "BilinearTools.h"
 
 void OutputStageProc::reset (float sampleRate)
 {
@@ -18,18 +19,15 @@ void OutputStageProc::calcCoefs (float curLevel)
     constexpr float C1 = (float) 4.7e-6;
 
     // analog coefficients
-    const auto b0s = C1 * R2;
-    const auto b1s = 0.0f;
-    const auto a0s = C1 * (R1 + R2);
-    const auto a1s = 1.0f;
+    float as[2], bs[2];
+    bs[0] = C1 * R2;
+    bs[1] = 0.0f;
+    as[0] = C1 * (R1 + R2);
+    as[1] = 1.0f;
 
     // bilinear transform
     const auto K = 2.0f * fs;
-    const auto a0 = a0s * K + a1s;
-    b[0] = ( b0s * K + b1s) / a0;
-    b[1] = (-b0s * K + b1s) / a0;
-    a[0] = 1.0f;
-    a[1] = (-a0s * K + a1s) / a0;
+    Bilinear::BilinearTransform<float, 2>::call (b, a, bs, as, K);
 }
 
 void OutputStageProc::processBlock (float* block, const int numSamples) noexcept
