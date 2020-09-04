18e90c9dae693f95c98be37d0957241e242fb09f
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2020-09-04 00:02:43 -0700

    Set up original models with new framework

--- ChowCentaur/GainStageMLProc.cpp
+++ ChowCentaur/GainStageMLProc.cpp
@@ -2,11 +2,13 @@
 
 GainStageMLProc::GainStageMLProc (AudioProcessorValueTreeState& vts)
 {
-    loadModel (gainStageMLSmall, BinaryData::centaur_small_json, BinaryData::centaur_small_jsonSize);
-    loadModel (gainStageMLLarge, BinaryData::centaur_json, BinaryData::centaur_jsonSize);
+    loadModel (gainStageML[0], BinaryData::centaur_0_json, BinaryData::centaur_0_jsonSize);
+    loadModel (gainStageML[1], BinaryData::centaur_25_json, BinaryData::centaur_25_jsonSize);
+    loadModel (gainStageML[2], BinaryData::centaur_50_json, BinaryData::centaur_50_jsonSize);
+    loadModel (gainStageML[3], BinaryData::centaur_75_json, BinaryData::centaur_75_jsonSize);
+    loadModel (gainStageML[4], BinaryData::centaur_100_json, BinaryData::centaur_100_jsonSize);
 
     gainParam = vts.getRawParameterValue ("gain");
-    mlParam     = vts.getRawParameterValue ("neural");
 }
 
 void GainStageMLProc::loadModel (ModelPtr model[2], const char* data, int size)
@@ -24,31 +26,49 @@ void GainStageMLProc::loadModel (ModelPtr model[2], const char* data, int size)
 
 void GainStageMLProc::reset (double sampleRate, int samplesPerBlock)
 {
-    ignoreUnused (samplesPerBlock);
+    ignoreUnused (sampleRate);
 
-    for (int ch = 0; ch < 2; ++ch)
-    {
-        gainStageMLSmall[ch]->reset();
-        gainStageMLLarge[ch]->reset();
-    }
+    for (int i = 0; i < numModels; ++i)
+        for (int ch = 0; ch < 2; ++ch)
+            gainStageML[i][ch]->reset();
 
-    T = 1.0f / (float) sampleRate;
+    fadeBuffer.setSize (2, samplesPerBlock);
+
+    lastModelIdx = getModelIdx();
 }
 
-void GainStageMLProc::processBlock (AudioBuffer<float>& buffer)
+void GainStageMLProc::processModel (AudioBuffer<float>& buffer, ModelPtr model[2])
 {
-    auto model = gainStageMLSmall;
-    if (*mlParam == 2)
-        model = gainStageMLLarge;
-
     for (int ch = 0; ch < buffer.getNumChannels(); ++ch)
     {
         auto* x = buffer.getWritePointer (ch);
 
         for (int n = 0; n < buffer.getNumSamples(); ++n)
         {
-            float input[] = { x[n], *gainParam, T };
+            float input[] = { x[n] };
             x[n] = model[ch]->forward (input);
         }
     }
+}
+
+void GainStageMLProc::processBlock (AudioBuffer<float>& buffer)
+{
+    const auto modelIdx = getModelIdx();
+
+    if (modelIdx == lastModelIdx)
+    {
+        processModel (buffer, gainStageML[modelIdx]);
+    }
+    else // need to fade between models
+    {
+        fadeBuffer.makeCopyOf (buffer, true);
+        processModel (buffer, gainStageML[lastModelIdx]); // previous model
+        processModel (fadeBuffer, gainStageML[modelIdx]); // next model
+
+        buffer.applyGainRamp (0, buffer.getNumSamples(), 1.0f, 0.0f);
+        for (int ch = 0; ch < buffer.getNumChannels(); ++ch)
+            buffer.addFromWithRamp (ch, 0, fadeBuffer.getReadPointer (ch), buffer.getNumSamples(), 0.0f, 1.0f);
+    }
+
+    lastModelIdx = modelIdx;
 }
--- ChowCentaur/GainStageMLProc.h
+++ ChowCentaur/GainStageMLProc.h
@@ -13,16 +13,25 @@ public:
     void processBlock (AudioBuffer<float>& buffer);
 
 private:
+    enum
+    {
+        numModels = 5,
+    };
+
     using ModelPtr = std::unique_ptr<Model<float>>;
     void loadModel (ModelPtr model[2], const char* data, int size);
+    void processModel (AudioBuffer<float>& buffer, ModelPtr model[2]);
 
+    inline int getModelIdx() const noexcept
+    { 
+        return jlimit (0, 4, int (numModels * *gainParam));
+    }
+
+    ModelPtr gainStageML[numModels][2];
+
+    AudioBuffer<float> fadeBuffer;
     std::atomic<float>* gainParam = nullptr;
-    std::atomic<float>* mlParam = nullptr;
-
-    ModelPtr gainStageMLSmall[2];
-    ModelPtr gainStageMLLarge[2];
-
-    float T = 1.0f / 44100.0f;
+    int lastModelIdx = 0;
 };
 
 #endif // GAINSTAGEMLPROC_H_INCLUDED
