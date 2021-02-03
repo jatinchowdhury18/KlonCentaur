ffdf7f0a6a5e3492837ff11f8829b176367c0b29
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2021-02-03 15:46:01 -0800

    Add Bypass parameter (#13)
    * Set up UI for bypass switch
    
    * Add bypass DSP
    
    * {Apply clang-format}
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- ChowCentaur/CustomLNFs.h
+++ ChowCentaur/CustomLNFs.h
@@ -35,3 +35,35 @@ public:
 private:
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ComboBoxLNF)
 };
+
+class ButtonLNF : public chowdsp::ChowLNF
+{
+public:
+    ButtonLNF() {}
+
+    void drawButtonBackground (Graphics& g, Button& button, const Colour& backgroundColour, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override
+    {
+        constexpr auto cornerSize = 3.0f;
+        auto bounds = button.getLocalBounds().toFloat().reduced (0.5f, 0.5f);
+
+        auto baseColour = backgroundColour.withMultipliedSaturation (button.hasKeyboardFocus (true) ? 1.3f : 0.9f)
+                              .withMultipliedAlpha (button.isEnabled() ? 1.0f : 0.5f);
+
+        if (shouldDrawButtonAsDown || shouldDrawButtonAsHighlighted)
+            baseColour = baseColour.contrasting (shouldDrawButtonAsDown ? 0.2f : 0.05f);
+
+        g.setColour (baseColour);
+        g.fillRoundedRectangle (bounds, cornerSize);
+
+        g.setColour (Colour (0xFF2B3335));
+        g.drawRoundedRectangle (bounds, cornerSize, 1.0f);
+    }
+
+    Font getTextButtonFont (TextButton&, int buttonHeight) override
+    {
+        return Font (jmin (17.0f, (float) buttonHeight * 0.8f)).boldened();
+    }
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ButtonLNF)
+};
