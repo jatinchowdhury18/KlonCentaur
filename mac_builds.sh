486add45ea2b2091193ecdade759f457c64546fc
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-02-09 00:53:15 -0500

    Minor updates

--- mac_builds.sh
+++ mac_builds.sh
@@ -14,26 +14,46 @@ sed -i '' '5s/#//' ChowCentaur/CMakeLists.txt
 # cmake new builds
 TEAM_ID=$(more ~/Developer/mac_id)
 cmake -Bbuild -GXcode -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY="Apple Distribution" \
-    -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=$TEAM_ID \
+    -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM="$TEAM_ID" \
     -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_STYLE="Manual" \
     -D"CMAKE_OSX_ARCHITECTURES=arm64;x86_64"
-cmake --build build --config Release -j8 | xcpretty
+cmake --build build --config Release -j12 | xcpretty
 
 # copy builds to bin
 mkdir -p bin/Mac
-declare -a plugins=("ChowCentaur")
-for plugin in "${plugins[@]}"; do
-    cp -R build/${plugin}/${plugin}_artefacts/Release/Standalone/${plugin}.app bin/Mac/${plugin}.app
-    cp -R build/${plugin}/${plugin}_artefacts/Release/VST/${plugin}.vst bin/Mac/${plugin}.vst
-    cp -R build/${plugin}/${plugin}_artefacts/Release/VST3/${plugin}.vst3 bin/Mac/${plugin}.vst3
-    cp -R build/${plugin}/${plugin}_artefacts/Release/AU/${plugin}.component bin/Mac/${plugin}.component
-done
+echo "Copying build artifacts..."
+plugin="ChowCentaur"
+cp -R build/${plugin}/${plugin}_artefacts/Release/Standalone/${plugin}.app bin/Mac/${plugin}.app
+cp -R build/${plugin}/${plugin}_artefacts/Release/VST/${plugin}.vst bin/Mac/${plugin}.vst
+cp -R build/${plugin}/${plugin}_artefacts/Release/VST3/${plugin}.vst3 bin/Mac/${plugin}.vst3
+cp -R build/${plugin}/${plugin}_artefacts/Release/AU/${plugin}.component bin/Mac/${plugin}.component
 
 # reset CMakeLists.txt
+echo "Restoring CMake configuration..."
 git restore CMakeLists.txt
 git restore ChowCentaur/CMakeLists.txt
 
+# run auval
+echo "Running AU validation..."
+cp -R build/${plugin}/${plugin}_artefacts/Release/AU/${plugin}.component ~/Library/Audio/Plug-Ins/Components/${plugin}.component
+manu=$(cut -f 6 -d ' ' <<< "$(grep 'PLUGIN_MANUFACTURER_CODE' ChowCentaur/CMakeLists.txt)")
+code=$(cut -f 6 -d ' ' <<< "$(grep 'PLUGIN_CODE' ChowCentaur/CMakeLists.txt)")
+
+set +e
+auval_result=$(auval -v aufx "$code" "$manu")
+auval_code="$?"
+echo "AUVAL code: $auval_code"
+
+if [ "$auval_code" != 0 ]; then
+    echo "$auval_result"
+    echo "auval FAIL!!!"
+    # exit 1
+else
+    echo "auval PASSED"
+fi
+
 # zip builds
+echo "Zipping builds..."
 VERSION=$(cut -f 2 -d '=' <<< "$(grep 'CMAKE_PROJECT_VERSION:STATIC' build/CMakeCache.txt)")
 (
     cd bin
