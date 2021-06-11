30ba016d84c32eb580940b60fa237a0a74a73dcb
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2021-06-10 22:50:22 -0700

    Update Windows build scripts for ASIO

--- win_builds.sh
+++ win_builds.sh
@@ -3,12 +3,12 @@
 set -e
 
 build64(){
-    cmake -Bbuild -G"Visual Studio 15 2017 Win64"
+    cmake -Bbuild -G"Visual Studio 16 2019" -A x64 -DASIOSDK_DIR="C:\\SDKs\\ASIO_SDK"
     cmake --build build --config Release -j4
 }
 
 build32(){
-    cmake -Bbuild32 -G"Visual Studio 15 2017"
+    cmake -Bbuild32 -G"Visual Studio 16 2019" -A Win32 -DASIOSDK_DIR="C:\\SDKs\\ASIO_SDK"
     cmake --build build32 --config Release -j4
 }
 
@@ -18,7 +18,7 @@ rm -Rf build32/
 rm -Rf bin/*Win64*
 rm -Rf bin/*Win32*
 
-# set up VST and ASIO paths
+# set up VST SDK path
 sed -i -e "s~# juce_set_vst2_sdk_path.*~juce_set_vst2_sdk_path(C:/SDKs/VST_SDK/VST2_SDK/)~" CMakeLists.txt
 
 # cmake new builds
